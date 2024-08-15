4b8239d74634c758d67e9a984120e63248793041
Letícia Kauanny de Oliveira Gonçalves <20231170150025@ifrn.local>
2024-08-15 16:45:13 -0300

    Feat: adicionando Quest09

---
+++ b/Quest09.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Quest09
+{
+	static void Main(string[] args)
+	{
+		int soma = 0;
+		int numero;
+
+		do
+		{
+			Console.WriteLine("Digite um número (ou 0 para sair):");
+			numero = int.Parse(Console.ReadLine());
+			soma += numero;
+		} while (numero != 0);
+
+		Console.WriteLine("A soma de todos os números digitados é: " + soma);
+	}
+}
