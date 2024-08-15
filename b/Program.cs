e7505421d357ed732973a3ddfbee965aa7751215
Letícia Kauanny de Oliveira Gonçalves <20231170150025@ifrn.local>
2024-08-15 16:18:06 -0300

    Feat: adicionando os arquivos

---
+++ b/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Digite o primeiro número:");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número:");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o terceiro número:");
+        int num3 = int.Parse(Console.ReadLine());
+
+        
+        int maior = num1;
+        if (num2 > maior)
+        {
+            maior = num2;
+        }
+        if (num3 > maior)
+        {
+            maior = num3;
+        }
+
+        
+        int menor = num1;
+        if (num2 < menor)
+        {
+            menor = num2;
+        }
+        if (num3 < menor)
+        {
+            menor = num3;
+        }
+
+        
+        Console.WriteLine("O maior número é: " + maior);
+        Console.WriteLine("O menor número é: " + menor);
